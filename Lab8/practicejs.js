d0d9409c1cf963ad3d419bf04a768b8c358007d9
rwilkinsbrock <90411160+rwilkinsbrock@users.noreply.github.com>
2021-12-12 17:53:14 -0500

    lab8 practice

---
+++ Lab8/practicejs.js
@@ -0,0 +1,11 @@
+document.write("Hello World!");
+
+
+
+document.write("<p> I love monkeys </p>");
+
+document.write(“Hello World”);
+
+document.getElementById(‘one’).style
+document.getElementById(‘two’).style
+document.getElementById(‘three’).style
