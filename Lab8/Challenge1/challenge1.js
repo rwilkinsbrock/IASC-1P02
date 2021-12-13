2992511e0d8c376fa931238646f2ae73f7e4748e
rwilkinsbrock <90411160+rwilkinsbrock@users.noreply.github.com>
2021-12-12 23:14:36 -0500

    Update challenge1.js

--- Lab8/Challenge1/challenge1.js
+++ Lab8/Challenge1/challenge1.js
@@ -1,5 +1,5 @@
 //A Function to printout Hurrah! when called
 
 function pushMe(){
-	document.getElementById("output").innerHTML = "Hurrah!";
+	document.getElementById("output").innerHTML = "Hurrah!" ;
 }
